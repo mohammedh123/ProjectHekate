1dde0303bc8d8ceda4197b1673bda871b48d4e68
Mohammed Hossain <mohammedh123@gmail.com>
2014-04-01 21:13:34 -0400

    Removes a lot of unused nodes from the grammar.

--- ProjectHekate.Scripting/BulletGrammar.cs
+++ ProjectHekate.Scripting/BulletGrammar.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using Irony.Ast;
 using Irony.Parsing;
+using Irony.Interpreter;
+using Irony.Interpreter.Ast;
 
 namespace ProjectHekate.Scripting
 {
@@ -26,25 +29,15 @@ namespace ProjectHekate.Scripting
 
             // symbols
             var semi = ToTerm(";", "semicolon");
-            var semiOpt = new NonTerminal("semi?") {Rule = Empty | semi};
             var dot = ToTerm(".", "dot");
             var comma = ToTerm(",", "comma");
-            var commaOpt = new NonTerminal("comma?") {Rule = Empty | comma};
             var commasOpt = new NonTerminal("commasOpt");
             commasOpt.Rule = MakeStarRule(commasOpt, null, comma);
             var lbr = ToTerm("{");
             var rbr = ToTerm("}");
             var lpar = ToTerm("(");
             var rpar = ToTerm(")");
-
-            // basics
-            var typeRef = new NonTerminal("typeRef");
-            var typeRefList = new NonTerminal("typeRefList");
-            var builtinType = new NonTerminal("builtinType");
-            var typeOrVoid = new NonTerminal("typeOrVoid");
-            var identifierOrBuiltIn = new NonTerminal("identifierOrBuiltIn");
-            var integralType = new NonTerminal("integralType");
-
+            
             // expressions
             var argument = new NonTerminal("argument");
             var argumentList = new NonTerminal("argumentList");
@@ -54,59 +47,43 @@ namespace ProjectHekate.Scripting
             var expression = new NonTerminal("expression", "expression");
             var expressionList = new NonTerminal("expressionList", "expressionList");
             var expressionOpt = new NonTerminal("expressionOpt");
-            var binOpExpression = new NonTerminal("binOpExpression");
             var unaryOperator = new NonTerminal("unaryOperator");
+            var binOperator = new NonTerminal("binOp", "operator symbol");
             var assignmentOperator = new NonTerminal("assignmentOperator");
-            var primaryExpression = new NonTerminal("primaryExpression");
-            var unaryExpression = new NonTerminal("unaryExpression");
-            var preIncrDecrExpression = new NonTerminal("preIncrDecrExpression");
-            var postIncrDecrExpression = new NonTerminal("postIncrDecrExpression");
-            var literal = new NonTerminal("literal");
+            var literal = new NonTerminal("literal", typeof(LiteralValueNode));
             var incrOrDecr = new NonTerminal("incrOrDecr");
             var incrOrDecrOpt = new NonTerminal("incrOrDecrOpt");
+            var memberAccess = new NonTerminal("memberAccess", typeof(MemberAccessNode));
+
             var parenthesizedExpression = new NonTerminal("parenthesizedExpression");
-            var memberAccess = new NonTerminal("memberAccess");
-            var memberAccessSegment = new NonTerminal("memberAccessSegment");
-            var memberAccessSegmentsOpt = new NonTerminal("memberAccessSegmentsOpt");
-            var binOp = new NonTerminal("binOp", "operator symbol");
-
-            var elemInitializer = new NonTerminal("elemInitializer");
-            var elemInitializerList = new NonTerminal("elemInitializerList");
-            var elemInitializerListExt = new NonTerminal("elemInitializerExt");
-            var initializerValue = new NonTerminal("initializerValue");
-
+            var unaryExpression = new NonTerminal("unaryExpression", typeof(UnaryOperationNode));
+            var preIncrDecrExpression = new NonTerminal("preIncrDecrExpression", typeof(IncDecNode));
+            var postIncrDecrExpression = new NonTerminal("postIncrDecrExpression", typeof(IncDecNode));
+            var primaryExpression = new NonTerminal("primaryExpression");
+            var binOpExpression = new NonTerminal("binOpExpression", typeof(BinaryOperationNode));
+            
             // statements
             var statement = new NonTerminal("statement", "statement");
-            var statementList = new NonTerminal("statementList");
+            var statementList = new NonTerminal("statementList", typeof(StatementListNode));
             var statementListOpt = new NonTerminal("statementListOpt");
             var declarationStatement = new NonTerminal("declarationStatement");
             var embeddedStatement = new NonTerminal("embeddedStatement");
-            var selectionStatement = new NonTerminal("selectionStatement");
             var iterationStatement = new NonTerminal("iterationStatement");
+            var assignmentStatement = new NonTerminal("assignmentStatement", typeof(AssignmentNode));
             var statementExpression = new NonTerminal("statementExpression");
             var statementExpressionList = new NonTerminal("statementExpressionList");
             var localVariableDeclaration = new NonTerminal("localVariableDeclaration");
-            var localConstDeclaration = new NonTerminal("localConstDeclaration");
-            var localVariableType = new NonTerminal("localVariableType");
-            var localVariableDeclarator = new NonTerminal("localVariableDeclarator");
-            var localVariableDeclarators = new NonTerminal("localVariableDeclarators");
+            var functionCallStatement = new NonTerminal("functionCallExpression");
             var initializeDeclaration = new NonTerminal("initializeDeclaration");
-            var block = new NonTerminal("block");
-            var ifStatement = new NonTerminal("ifStatement");
+            var block = new NonTerminal("block", typeof(StatementListNode));
+            var ifStatement = new NonTerminal("ifStatement", typeof(IfNode));
             var elseClauseOpt = new NonTerminal("elseClauseOpt");
             var whileStatement = new NonTerminal("whileStatement");
-            var forStatement = new NonTerminal("forStatement");
-            var forInitializerOpt = new NonTerminal("forInitializerOpt");
-            var forConditionOpt = new NonTerminal("forConditionOpt");
-            var forIteratorOpt = new NonTerminal("forIteratorOpt");
             var breakStatement = new NonTerminal("breakStatement");
-            var continueStatement = new NonTerminal("continueStatement");
-            var returnStatement = new NonTerminal("returnStatement");
 
             // top-level objects and stuff
             var emitterDeclarationList = new NonTerminal("emitterDeclarationList");
             var emitterDeclaration = new NonTerminal("emitterDeclaration");
-            var qualifiedIdentifier = new NonTerminal("qualifiedIdentifier");
             var emitterBody = new NonTerminal("emitterBody");
             var emitterMemberDeclaration = new NonTerminal("emitterMemberDeclaration");
             var emitterMemberDeclarations = new NonTerminal("emitterMemberDeclarations");
@@ -117,18 +94,11 @@ namespace ProjectHekate.Scripting
             // emitter stuff
             var memberDeclaration = new NonTerminal("memberDeclaration");
             var memberDeclarationsOpt = new NonTerminal("memberDeclarationsOpt");
-            var constantDeclaration = new NonTerminal("constantDeclaration");
-            var constantDeclarator = new NonTerminal("constantDeclarator");
-            var constantDeclarators = new NonTerminal("constantDeclarators");
-            var fieldDeclaration = new NonTerminal("fieldDeclaration");
+            var fieldDeclaration = new NonTerminal("fieldDeclaration", typeof(FieldAstNode));
             var functionDeclaration = new NonTerminal("functionDeclaration");
-            var variableDeclarator = new NonTerminal("variableDeclarator");
-            var variableDeclarators = new NonTerminal("variableDeclarators");
-            var functionBody = new NonTerminal("methodBody");
-            var formalParameterList = new NonTerminal("formalParameterList");
+            var functionBody = new NonTerminal("functionBody");
+            var formalParameterList = new NonTerminal("formalParameterList", typeof(ParamListNode));
             var formalParameterListPar = new NonTerminal("formalParameterListPar");
-            var fixedParameter = new NonTerminal("fixedParameter");
-            var fixedParameters = new NonTerminal("fixedParameters");
 
             // operators
             RegisterOperators(1, "||");
@@ -146,8 +116,8 @@ namespace ProjectHekate.Scripting
 
             MarkPunctuation(";", ",", "(", ")", "{", "}", "[", "]", ":");
 
-            MarkTransient(emitterMemberDeclaration, memberDeclaration, statement, embeddedStatement, expression, literal, binOp,
-                primaryExpression, expression);
+            MarkTransient(emitterMemberDeclaration, memberDeclaration, statement, embeddedStatement, expression, literal, binOperator,
+                primaryExpression, statementListOpt, argumentListPar, argumentListOpt, argument, statementExpression, memberAccess, functionBody, incrOrDecr, declarationStatement, iterationStatement, parenthesizedExpression);
 
             AddTermsReportGroup("assignment", "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=");
             AddTermsReportGroup("typename", "bool", "float", "string", "int", "char");
@@ -160,15 +130,7 @@ namespace ProjectHekate.Scripting
 
             AddToNoReportGroup(comma, semi);
             AddToNoReportGroup("const", "++", "--", "this", "{", "}", "[");
-
-            // types
-            typeRef.Rule = typeOrVoid;
-            typeRefList.Rule = MakePlusRule(typeRefList, comma, typeRef);
-            integralType.Rule = ToTerm("int") | "char";
-            typeOrVoid.Rule = identifierOrBuiltIn | "void";
-            builtinType.Rule = integralType | "bool" | "float" | "string";
-            identifierOrBuiltIn.Rule = identifier | builtinType;
-
+            
             // expressions
             expression.Rule = binOpExpression | primaryExpression;
             expressionOpt.Rule = Empty | expression;
@@ -181,7 +143,7 @@ namespace ProjectHekate.Scripting
             unaryOperator.Rule = ToTerm("+") | "-" | "!" | "~" | "*";
             assignmentOperator.Rule = ToTerm("=") | "+=" | "-=" | "*=" | "/=" | "%=" | "&=" | "|=" | "^=" | "<<=" | ">>=";
 
-            binOpExpression.Rule = expression + binOp + expression;
+            binOpExpression.Rule = expression + binOperator + expression;
 
             unaryExpression.Rule = unaryOperator + primaryExpression;
             primaryExpression.Rule =
@@ -197,52 +159,35 @@ namespace ProjectHekate.Scripting
             preIncrDecrExpression.Rule = incrOrDecr + memberAccess;
             postIncrDecrExpression.Rule = memberAccess + incrOrDecr;
 
-            memberAccess.Rule = identifierOrBuiltIn + memberAccessSegmentsOpt;
-            memberAccessSegmentsOpt.Rule = MakeStarRule(memberAccessSegmentsOpt, null, memberAccessSegment);
-            memberAccessSegment.Rule = dot + identifier
-                                       | argumentListPar;
-            elemInitializer.Rule = initializerValue | identifier + "=" + initializerValue;
-            elemInitializerList.Rule = MakePlusRule(elemInitializerList, comma, elemInitializer);
-            elemInitializerListExt.Rule = Empty | elemInitializerList + commaOpt;
-            initializerValue.Rule = expression;
+            memberAccess.Rule = identifier;
             argumentListPar.Rule = lpar + argumentListOpt + rpar;
             argumentListParOpt.Rule = Empty | argumentListPar;
 
-            initializerValue.Rule = expression;
-            binOp.Rule = ToTerm("<") | "||" | "&&" | "|" | "^" | "&" | "==" | "!=" | ">" | "<=" | ">=" | "<<" | ">>" | "+" | "-" | "*" | "/" |
+            binOperator.Rule = ToTerm("<") | "||" | "&&" | "|" | "^" | "&" | "==" | "!=" | ">" | "<=" | ">=" | "<<" | ">>" | "+" | "-" | "*" | "/" |
                          "%" | "=" | "+=" | "-=" | "*=" | "/=" | "%=" | "&=" | "|=" | "^=" | "<<=" | ">>=";
 
             // statements
-            statement.Rule = declarationStatement | embeddedStatement;
+            statement.Rule = declarationStatement | embeddedStatement | functionCallStatement;
             statement.ErrorRule = SyntaxError + semi;
             statementList.Rule = MakeListRule(statementList, null, statement);
             statementListOpt.Rule = Empty | statementList;
 
-            declarationStatement.Rule = localVariableDeclaration + semi | localConstDeclaration + semi;
-            localVariableDeclaration.Rule = localVariableType + localVariableDeclarators;
-            localVariableType.Rule = memberAccess;
-            localVariableDeclarator.Rule = identifier | identifier + "=" + initializerValue;
-            localVariableDeclarators.Rule = MakePlusRule(localVariableDeclarators, comma, localVariableDeclarator);
-            localConstDeclaration.Rule = "const" + typeRef + constantDeclarator;
+            functionCallStatement.Rule = identifier + argumentListPar;
+            declarationStatement.Rule = localVariableDeclaration + semi;
+            localVariableDeclaration.Rule = identifier + "=" + expression;
 
-            embeddedStatement.Rule = block | semi | statementExpression + semi | selectionStatement | iterationStatement;
+            embeddedStatement.Rule = block | semi | statementExpression + semi | ifStatement | iterationStatement;
             block.Rule = lbr + statementListOpt + rbr;
 
-            selectionStatement.Rule = ifStatement;
             ifStatement.Rule = ToTerm("if") + lpar + expression + rpar + embeddedStatement + elseClauseOpt;
             elseClauseOpt.Rule = Empty | PreferShiftHere() + "else" + embeddedStatement;
 
-            iterationStatement.Rule = whileStatement | forStatement;
+            iterationStatement.Rule = whileStatement;
             whileStatement.Rule = "while" + parenthesizedExpression + embeddedStatement;
-            forStatement.Rule = "for" + lpar + forInitializerOpt + semi + forConditionOpt + semi + forIteratorOpt + rpar + embeddedStatement;
-            forInitializerOpt.Rule = Empty | localVariableDeclaration | statementExpressionList;
-            forConditionOpt.Rule = Empty | expression;
-            forIteratorOpt.Rule = Empty | statementExpressionList;
             breakStatement.Rule = "break" + semi;
-            continueStatement.Rule = "continue" + semi;
-            returnStatement.Rule = "return" + expressionOpt + semi;
 
-            statementExpression.Rule = memberAccess | memberAccess + assignmentOperator + expression | preIncrDecrExpression |
+            assignmentStatement.Rule = memberAccess + assignmentOperator + expression;
+            statementExpression.Rule = memberAccess | assignmentStatement | preIncrDecrExpression |
                                        postIncrDecrExpression;
             statementExpressionList.Rule = MakePlusRule(statementExpressionList, comma, statementExpression);
             incrOrDecrOpt.Rule = Empty | ToTerm("++") | "--";
@@ -250,9 +195,8 @@ namespace ProjectHekate.Scripting
 
             Root = emitterDeclarationList;
             emitterDeclarationList.Rule = MakePlusRule(emitterDeclarationList, null, emitterDeclaration);
-            emitterDeclaration.Rule = "emitter" + qualifiedIdentifier + emitterBody + semiOpt;
-            qualifiedIdentifier.Rule = MakePlusRule(qualifiedIdentifier, dot, identifier);
-            emitterBody.Rule = "{" + emitterMemberDeclarations + initializeDeclaration + emitterMemberDeclarations + "}";
+            emitterDeclaration.Rule = "emitter" + identifier + emitterBody;
+            emitterBody.Rule = "{" + emitterMemberDeclarations + initializeDeclaration + "}";
             emitterMemberDeclaration.Rule = memberDeclaration | stateDeclaration;
             emitterMemberDeclarations.Rule = MakeStarRule(emitterMemberDeclarations, null, emitterMemberDeclaration);
             initializeDeclaration.Rule = "Initialize" + block;
@@ -260,25 +204,20 @@ namespace ProjectHekate.Scripting
             stateDeclaration.Rule = "state" + identifier + stateBody;
             stateBody.Rule = lbr + memberDeclarationsOpt + rbr;
 
-            memberDeclaration.Rule = constantDeclaration | fieldDeclaration | functionDeclaration;
+            memberDeclaration.Rule = fieldDeclaration | functionDeclaration;
             memberDeclaration.ErrorRule = SyntaxError + ";" | SyntaxError + "}";
             memberDeclarationsOpt.Rule = MakeStarRule(memberDeclarationsOpt, null, memberDeclaration);
-            constantDeclaration.Rule = "const" + typeRef + constantDeclarators + semi;
-            constantDeclarator.Rule = identifier + "=" + expression;
-            constantDeclarators.Rule = MakePlusRule(constantDeclarators, comma, constantDeclarator);
-            fieldDeclaration.Rule = builtinType + variableDeclarators + semi;
-            variableDeclarator.Rule = identifier | identifier + "=" + elemInitializer;
-            variableDeclarators.Rule = MakePlusRule(variableDeclarators, comma, variableDeclarator);
+            fieldDeclaration.Rule = localVariableDeclaration + semi;
 
-            functionDeclaration.Rule = "function" + typeRef + identifierOrBuiltIn + formalParameterListPar + functionBody;
-            formalParameterList.Rule = fixedParameters;
+            functionDeclaration.Rule = "function" + identifier + formalParameterListPar + functionBody;
+            formalParameterList.Rule = MakePlusRule(formalParameterList, comma, identifier);
             formalParameterListPar.Rule = lpar + rpar | lpar + formalParameterList + rpar;
-            fixedParameter.Rule = typeRef + identifier;
-            fixedParameters.Rule = MakePlusRule(fixedParameters, comma, fixedParameter);
-            functionBody.Rule = block | semi;
+            functionBody.Rule = block;
 
             //Prepare term set for conflict resolution
             _skipTokensInPreview.UnionWith(new Terminal[] { dot, identifier, comma, ToTerm("::"), comma, ToTerm("["), ToTerm("]") });
+
+            //LanguageFlags = LanguageFlags.CreateAst;
         }
     }
 }
