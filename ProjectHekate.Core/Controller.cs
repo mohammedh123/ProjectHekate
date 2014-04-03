7eb3bd43ba9f71bd8b8cd307d77c6b4ed554a751
Mohammed Hossain <mohammedh123@gmail.com>
2014-04-03 01:28:42 -0400

    Adds the basics of a Controller class (holds emitters) and the core engine class.

---
+++ ProjectHekate.Core/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHekate.Core
+{
+    /// <summary>
+    /// Controllers are objects that have emitters. The controller is disabled by default.
+    /// </summary>
+    public interface IController
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        float Angle { get; set; }
+        bool Enabled { get; set; }
+
+        void AddEmitter(IEmitter e);
+    }
+
+    class Controller : IController
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Angle { get; set; }
+        public bool Enabled { get; set; }
+
+        private readonly List<IEmitter> _emitters = new List<IEmitter>();
+
+
+        internal Controller()
+        {}
+
+        public void AddEmitter(IEmitter e)
+        {
+            _emitters.Add(e);
+        }
+    }
+}
---
+++ ProjectHekate.Core/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHekate.Core
+{
+    public interface IEngine
+    {
+        IController CreateController(float x, float y, float angle, bool enabled);
+        IEmitter CreateEmitter(float x, float y, float angle, bool enabled, EmitterUpdateDelegate updater);
+    }
+
+    public class Engine : IEngine
+    {
+        public IController CreateController(float x, float y, float angle, bool enabled)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmitter CreateEmitter(float x, float y, float angle, bool enabled, EmitterUpdateDelegate updater)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
