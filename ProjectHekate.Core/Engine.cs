430352e8e44d5248f54045a0bc037e51c945e156
Mohammed Hossain <mohammedh123@gmail.com>
2014-04-18 12:05:19 -0400

    Moves ControllerBuilder out of the Engine file (and fixes it up a bit).

--- ProjectHekate.Core/Engine.cs
+++ ProjectHekate.Core/Engine.cs
@@ -15,57 +15,6 @@ namespace ProjectHekate.Core
         void Update(float dt);
     }
     
-
-    public class ControllerBuilder
-    {
-        private Controller _controller;
-        private List<Emitter> _emitters; 
-        private Engine _engine;
-
-        internal static EmitterUpdateWithInterpolationDelegate ConvertUpdateDelegateToInterpolationDelegate(EmitterUpdateDelegate updater)
-        {
-            return (e, bs, ins) => updater(e, bs);
-        }
-
-        internal ControllerBuilder(float x, float y, float angle, bool enabled, Engine e)
-        {
-            _controller = new Controller()
-                          {
-                              X = x,
-                              Y = y,
-                              Angle = angle,
-                              Enabled = enabled
-                          };
-            _emitters = new List<Emitter>();
-            _engine = e;
-        }
-
-        public ControllerBuilder WithEmitter(float x, float y, float angle, bool enabled, EmitterUpdateDelegate updater)
-        {
-            var emitter = new Emitter() { X = x, Y = y, Angle = angle, Enabled = enabled, UpdateFunc = ConvertUpdateDelegateToInterpolationDelegate(updater) };
-            _controller.Emitters.Add(emitter);
-            _emitters.Add(emitter);
-
-            return this;
-        }
-
-        public ControllerBuilder WithEmitter(float x, float y, float angle, bool enabled, EmitterUpdateWithInterpolationDelegate updater)
-        {
-            var emitter = new Emitter() { X = x, Y = y, Angle = angle, Enabled = enabled, UpdateFunc = updater };
-            _controller.Emitters.Add(emitter);
-            _emitters.Add(emitter);
-
-            return this;
-        }
-
-        public IController Build()
-        {
-            _engine.AddController(_controller);
-            _engine.AddEmitters(_emitters);
-            return _controller;
-        }
-    }
-
     public class Engine : IEngine
     {
         private readonly BulletSystem _bulletSystem;
