efe8ff5186b73f6bb494384a16e450a0cc768558
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-04 23:29:55 -0400

    Fixes up the test and packages a bit.

--- ProjectHekate.Scripting.Tests/GrammarTests.cs
+++ ProjectHekate.Scripting.Tests/GrammarTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Antlr4.Runtime;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProjectHekate.Scripting.Tests
@@ -9,13 +10,15 @@ namespace ProjectHekate.Scripting.Tests
     public class GrammarTests
     {
         [TestMethod]
-        public void ShouldParseEmptyString()
+        public void ShouldParseEmptyStringToEmptyTree()
         {
             var input = new AntlrInputStream("");
             var lexer = new HekateGrammarLexer(input);
             var tokens = new CommonTokenStream(lexer);
             var parser = new HekateGrammarParser(tokens);
             var tree = parser.script();
+
+            tree.ChildCount.Should().Be(0);
         }
     }
 }
