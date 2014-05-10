b44d1324ed630e6a6f5f300fcdf6bc5624fdb3bc
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-09 21:01:10 -0400

    Adds some temporary records in the virtual machine that will store data about the code.

--- ProjectHekate.Scripting/VirtualMachine.cs
+++ ProjectHekate.Scripting/VirtualMachine.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ProjectHekate.Scripting
 {
+    public class ScriptRecord
+    {
+        public List<byte> Code { get; set; }
+
+        public ScriptRecord()
+        {
+            Code = new List<byte>();
+        }
+    }
+
+    public class BulletUpdaterScriptRecord : ScriptRecord
+    {
+    }
+
+    public class EmitterUpdaterScriptRecord : ScriptRecord
+    {
+    }
+
     public class VirtualMachine
     {
+        private readonly List<BulletUpdaterScriptRecord> _bulletUpdaterScriptRecords;
+
+
+        public VirtualMachine()
+        {
+            _bulletUpdaterScriptRecords = new List<BulletUpdaterScriptRecord>();
+        }
     }
 }
