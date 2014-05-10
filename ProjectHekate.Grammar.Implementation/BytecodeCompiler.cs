e8a327b096998a763cd1cc89791f0b1bdcf82e3c
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-09 21:01:48 -0400

    Adds a barebones compiler and the beginnings of a visitor for the tree.

---
+++ ProjectHekate.Grammar.Implementation/BytecodeCompiler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHekate.Grammar.Implementation
+{
+    public class BytecodeCompiler
+    {
+        public void Generate(HekateParser.ExpressionStatementContext context)
+        {
+            
+        }
+    }
+
+    class BytecodeBlock
+    {
+        public List<uint> Bytes { get; set; }
+
+        public BytecodeBlock()
+        {
+            Bytes = new List<uint>();
+        }
+    }
+}
---
+++ ProjectHekate.Grammar.Implementation/HekateScriptVisitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectHekate.Scripting;
+
+namespace ProjectHekate.Grammar.Implementation
+{
+
+    public class HekateScriptVisitor : HekateBaseVisitor<BytecodeCompiler>
+    {
+        private BytecodeCompiler _compiler;
+
+        public override BytecodeCompiler VisitScript(HekateParser.ScriptContext context)
+        {
+            // beginning of the script, create a new compiler that all children will use
+            _compiler = new BytecodeCompiler();
+
+            foreach (var child in context.children)
+            {
+                Visit(child);
+            }
+
+            return _compiler;
+        }
+
+        public override BytecodeCompiler VisitEmitterUpdaterDeclaration(HekateParser.EmitterUpdaterDeclarationContext context)
+        {
+            var bUpdaterRecord = new BulletUpdaterScriptRecord();
+
+            var name = context.Identifier().GetText();
+
+            foreach (var child in context.children) {
+                Visit(child);
+            }
+
+            return 1;
+        }
+
+        public override BytecodeCompiler VisitExpressionStatement(HekateParser.ExpressionStatementContext context)
+        {
+            var str = context.GetText();
+
+            return str;
+        }
+    }
+}
