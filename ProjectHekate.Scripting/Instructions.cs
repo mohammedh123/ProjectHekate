f1de389424b2deb2d2366ddaae700a9f8f6bd686
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-09 20:59:43 -0400

    Adds the first iteration of the instruction set for the virtual machine.

---
+++ ProjectHekate.Scripting/Instructions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHekate.Scripting
+{
+    enum Instructions
+    {
+        Push,
+        Pop,
+        OperatorNegative,
+        OperatorAdd,
+        OperatorSubtract,
+        OperatorMultiply,
+        OperatorDivide,
+        OperatorMod,
+        OperatorLessThan,
+        OperatorLessThanEqual,
+        OperatorGreaterThan,
+        OperatorGreaterThanEqual,
+        OperatorEqual,
+        OperatorNotEqual,
+        OperatorAnd,
+        OperatorOr,
+        Jump,
+        Compare,
+        FunctionCall,
+        GetProperty,
+        GetUpdater,
+        GetVariable,
+        SetVariable,
+        Fire,
+        WaitFrames
+    }
+}
