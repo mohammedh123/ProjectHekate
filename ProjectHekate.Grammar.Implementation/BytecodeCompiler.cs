1abf563a7415e8c3e5ff3db588e0f0687cf641b9
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-24 19:34:39 -0400

    Adds the skeleton of the bytecode compiler.

--- ProjectHekate.Grammar.Implementation/BytecodeCompiler.cs
+++ ProjectHekate.Grammar.Implementation/BytecodeCompiler.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectHekate.Scripting;
 
 namespace ProjectHekate.Grammar.Implementation
 {
+    class BytecodeCompiler
+    {
+        private readonly IVirtualMachine _virtualMachine;
+
+        public BytecodeCompiler(IVirtualMachine virtualMachine)
+        {
+            _virtualMachine = virtualMachine;
+        }
+
+        public CodeBlock GenerateConstantExpression(VirtualMachine vm)
+        {
+            return null;
+        }
+    }
 }
