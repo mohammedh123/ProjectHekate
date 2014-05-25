156b4e7891dfc40786c331c105f1f28777a60e36
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-24 20:49:13 -0400

    Adds skeleton test class for BytecodeCompiler.

---
+++ ProjectHekate.Grammar.Tests/TBytecodeCompiler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectHekate.Grammar.Tests
+{
+    [TestClass]
+    public class TBytecodeCompiler
+    {
+        [TestClass]
+        public class GenerateConstantExpression : TBytecodeCompiler
+        {
+             
+        }
+    }
+}
