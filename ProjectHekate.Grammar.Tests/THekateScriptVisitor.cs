da166e73f34068f9036f16aafc749a57099184d8
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-25 01:18:56 -0400

    Adds the skeleton tests for HekateScriptVisitor.

---
+++ ProjectHekate.Grammar.Tests/THekateScriptVisitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using AutoMoq.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProjectHekate.Grammar.Implementation;
+using ProjectHekate.Scripting;
+
+namespace ProjectHekate.Grammar.Tests
+{
+    [TestClass]
+    public class THekateScriptVisitor : AutoMoqTestFixture<HekateScriptVisitor>
+    {
+        [TestClass]
+        public class GenerateConstantExpression : THekateScriptVisitor
+        {
+            [TestClass]
+            public class Addition : THekateScriptVisitor
+            {
+                [TestMethod]
+                public void ShouldGenerateCodeForBasicExpression()
+                {
+                    // Setup: dummy data + mock vm out
+                    const string expression = "3+5";
+
+                    ResetSubject();
+
+                    // Act
+
+                    // Verify
+
+                    Assert.Inconclusive("Test not written yet.");
+                }
+
+                
+            }
+        }
+    }
+}
