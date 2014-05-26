4b0da65d71232d1949eff0c565a3a952dda1bdbb
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-26 13:19:09 -0400

    Adds an extension class with a method to get the first descendant of a specific type of a ParserRuleContext.

---
+++ ProjectHekate.Grammar.Implementation/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace ProjectHekate.Grammar.Implementation
+{
+    public static class Extensions
+    {
+        public static TNodeType GetFirstDescendantOfType<TNodeType>(this ParserRuleContext context) where TNodeType : class, IParseTree 
+        {
+            if (context == null) return null;
+            if (context is TNodeType) return context as TNodeType;
+
+            var contextQueue = new Queue<IParseTree>();
+            contextQueue.Enqueue(context);
+
+            while (contextQueue.Any()) {
+                var ctx = contextQueue.Dequeue();
+
+                for(var i = 0; i < ctx.ChildCount; i++) {
+                    var child = ctx.GetChild(i);
+
+                    if (child is TNodeType) return child as TNodeType;
+                    contextQueue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+    }
+}
