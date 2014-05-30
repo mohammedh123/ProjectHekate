749184598051cb5778803319099a4cdf0dd4ae35
Mohammed Hossain <mohammedh123@gmail.com>
2014-05-29 22:33:51 -0400

    Adds an interface and implementation for scope management.

---
+++ ProjectHekate.Grammar.Implementation/StackScopeManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectHekate.Scripting;
+
+namespace ProjectHekate.Grammar.Implementation
+{
+    public interface IScopeManager
+    {
+        void Add(CodeBlock scope);
+        void Remove();
+        CodeBlock GetCurrentScope();
+    }
+
+    class StackScopeManager : IScopeManager
+    {
+        private readonly Stack<CodeBlock> _scopeStack;
+
+
+        public StackScopeManager()
+        {
+            _scopeStack = new Stack<CodeBlock>();
+        }
+
+        public void Add(CodeBlock scope)
+        {
+            _scopeStack.Push(scope);
+        }
+
+        public void Remove()
+        {
+            _scopeStack.Pop();
+        }
+
+        public CodeBlock GetCurrentScope()
+        {
+            return _scopeStack.Peek();
+        }
+    }
+}
