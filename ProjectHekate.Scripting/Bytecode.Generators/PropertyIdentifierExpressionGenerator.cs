7680c80fffd007a1b5d85798d3d5d675b4ec9a24
Mohammed Hossain <mohammedh123@gmail.com>
2014-06-21 23:36:46 -0400

    Removes unused using statements.

--- ProjectHekate.Scripting/Bytecode.Generators/PropertyIdentifierExpressionGenerator.cs
+++ ProjectHekate.Scripting/Bytecode.Generators/PropertyIdentifierExpressionGenerator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ProjectHekate.Scripting.Helpers;
+﻿using ProjectHekate.Scripting.Helpers;
 using ProjectHekate.Scripting.Interfaces;
 
 namespace ProjectHekate.Scripting.Bytecode.Generators
